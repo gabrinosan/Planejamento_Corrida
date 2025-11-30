5aeb828ba63c7ac8b7c91161f4f2c73474dd2d98
gabrinosan <gabrielsh1854@gmail.com>
2025-11-30 18:01:16 -0300

    Redefinindo estrutura

--- tratamento.py
+++ tratamento.py
@@ -5,20 +5,24 @@ df = pd.read_csv("run_ww_2020_m.csv")
 # Removendo outliers e deixando apenas meio maratonistas
 df_rem_out = df[(df["duration"] <= 150) & (df["duration"] >= 50) & (df["distance"] >= 18) & (df["distance"] <= 22)]
 
-df_rem_out = df_rem_out.drop("major", axis=1)
+df_rem_out = df_rem_out.drop(["major", "athlete", "country"], axis=1).dropna()
 
-# print(df_rem_out[df_rem_out.isna().any(axis=1)])
+df_rem_out = df_rem_out.rename(columns={"Unnamed: 0": "id"})
 
-# print(df_rem_out.isna().any(axis=1).sum()) # Linhas que serão removidas (Nan)
+# print(df_rem_out.count()) # Verificando coerência entre quantidade e tipos
 
-df_rem_nan = df_rem_out.dropna() # Removendo valores Nan
+df_sep_M = df_rem_out[df_rem_out['gender'] == 'M']
+df_sep_M = df_sep_M.reset_index(drop=True)
+df_sep_M["id"] = df_sep_M.index
 
-print(df_rem_nan.head(5))
-# print(df_rem_nan[df_rem_nan.isna().any(axis=1)])
+df_sep_F = df_rem_out[df_rem_out['gender'] == 'F']
+df_sep_F = df_sep_F.reset_index(drop=True)
+df_sep_F["id"] = df_sep_F.index
 
-# print(df_rem_nan.count()) # Verificando coerência entre quantidade e tipos
+df_sep_F.to_csv("21Km_Mulheres.csv", index = False)
+df_sep_M.to_csv("21Km_Homens.csv", index = False)
 
-# print(df_rem_nan.dtypes)
-
+# print('Mulheres: ' + str(df_sep_F['gender'].count()), 'Homens: ' + str(df_sep_M['gender'].count()))
+print(df_sep_M.head())
 
 
